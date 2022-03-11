9c61c34865f0c33ea28a8c73ad3d693fa3ddedde
rgl-za <hyeonjeong2829@gmail.com>
2022-03-11 17:32:56 +0900

    Day6 Object Oriented Programming(3)

---
+++ Java/Object Oriented Programming/Pratice03/src/com/javaex/problem07/Account.java
@@ -0,0 +1,30 @@
+package com.javaex.day6;
+
+public class Account {
+
+	private String accountNo;
+    private int balance;
+    
+    
+    //생성자 작성
+    public Account(String accountNo) {
+		// TODO Auto-generated constructor stub
+    	System.out.println(accountNo + " 계좌가 개설되었습니다.");
+	}
+    
+    //필요한 메소드 작성
+    public void deposit(int money) {
+		// TODO Auto-generated method stub
+		balance += money;
+	}
+    
+    public void withdraw(int money) {
+    	balance -= money;
+    }
+
+	public void showBalance() {
+		// TODO Auto-generated method stub
+		System.out.println(balance);
+	}
+}
+
